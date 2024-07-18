45a63a006891da44f859599bc8a3f768e06db052
Jerome Roeser <jerome.roeser@gmail.com>
2024-07-18 11:26:16 +0200

    Add table type/shape and database_created tests -- should fail on push

--- tests/test_database.py
+++ tests/test_database.py
@@ -7,16 +7,16 @@ from world_handicap_calculator.main.main import calculate_index
 
 
 class TestDatabase(unittest.TestCase):
-    # def test_index_table_type(self):
-    #     index_table = calculate_index()
-    #     self.assertIsInstance(index_table, pd.DataFrame)
+    def test_index_table_type(self):
+        index_table = calculate_index()
+        self.assertIsInstance(index_table, pd.DataFrame)
 
-    # def test_index_table_shape(self):
-    #     index_table = calculate_index()
-    #     self.assertEqual(index_table.shape[1], 18)
+    def test_index_table_shape(self):
+        index_table = calculate_index()
+        self.assertEqual(index_table.shape[1], 18)
 
-    # def test_database_created(self):
-    #     self.assertTrue(Path(LOCAL_DATA_PATH).joinpath('golf.sqlite').exists())
+    def test_database_created(self):
+        self.assertTrue(Path(LOCAL_DATA_PATH).joinpath('golf.sqlite').exists())
 
     def test_folder_structure(self):
         self.assertTrue(Path(repo_path).joinpath("world_handicap_calculator", "api").exists())
